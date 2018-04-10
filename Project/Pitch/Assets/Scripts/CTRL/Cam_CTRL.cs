d0e3472cee904c3c454030d9971b2df40abca480
ErickFern <ferick@hotmail.de>
2018-04-10 19:31:15 +0200

    Optimized camera rotation - adjust y-axis more -

--- Project/Pitch/Assets/Scripts/CTRL/Cam_CTRL.cs
+++ Project/Pitch/Assets/Scripts/CTRL/Cam_CTRL.cs
@@ -2,37 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cam_CTRL : MonoBehaviour {
+
+public class Cam_CTRL : Game_CTRL {
 
     [SerializeField] private float lookSensitivity = 5;
     [SerializeField] private float lookSmoothDamp = 0.1f;
+
+    [Space(10, order = 1)]
     [SerializeField] private GameObject player;
 
-
+    #region rotationProperties
     private float xRotation;
     private float yRotation;
     private float currentXRotation;
     private float currentYRotation;
     private float xRotationV;
     private float yRotationV;
+    #endregion
 
+    #region positionProperties
+
+#endregion
+
+    private void Start()
+    {
+
+    }
 
     void Update () 
     {
+        if (player == null)
+            return;
 
-        yRotation += Input.GetAxis("Mouse X") /** lookSensitivity*/;
-        xRotation += Input.GetAxis("Mouse Y") /** lookSensitivity*/;
+        yRotation += Input.GetAxis("Mouse X") * lookSensitivity;
+        xRotation += Input.GetAxis("Mouse Y") * lookSensitivity;
 
-        //xRotation = Mathf.Clamp(xRotation, -90, 90);
-
-        currentXRotation = Mathf.SmoothDamp(currentXRotation, xRotation, ref xRotationV, lookSmoothDamp);
-        currentYRotation = Mathf.SmoothDamp(currentYRotation, yRotation, ref yRotationV, lookSmoothDamp);
+        this.transform.position += new Vector3(0, Input.GetAxis("Mouse Y") * lookSensitivity, 0);
+        this.transform.position = new Vector3(0,Mathf.Clamp(transform.localPosition.y, 0, 3), 0);
+        //currentXRotation = Mathf.SmoothDamp(currentXRotation, xRotation, ref xRotationV, lookSmoothDamp);
+        //currentYRotation = Mathf.SmoothDamp(currentYRotation, yRotation, ref yRotationV, lookSmoothDamp);
 
         player.transform.eulerAngles = new Vector3(player.transform.eulerAngles.x, yRotation, player.transform.eulerAngles.z);
-        transform.eulerAngles = new Vector3(xRotation, player.transform.eulerAngles.y, player.transform.eulerAngles.z);
+        //player.transform.eulerAngles = new Vector3(xRotation, 0, player.transform.eulerAngles.z);
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        yRotation = Mathf.Clamp(yRotation, -90f, 90f);
+
+
         this.transform.LookAt(player.transform);
-
-
-
     }
 }
--- Project/Pitch/Assets/Scripts/CTRL/Game_CTRL.cs
+++ Project/Pitch/Assets/Scripts/CTRL/Game_CTRL.cs
@@ -6,11 +6,22 @@ public class Game_CTRL : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+
+        Cursor.visible = false;
+        // CursorLockMode.
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    protected void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
 }
