01e526dbbac14fda045e307f83d298146334eb28
WilliamProg <ratz@web.de>
2018-04-13 19:03:09 +0200

    Readded stuff from backup
    stuff is death_collider, Testo, char_COLL and Game scene

---
+++ Project/Pitch/Assets/Scripts/Obstacles/Char_COLL1.cs
@@ -0,0 +1,27 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class GroundColl : MonoBehaviour {
+
+//    public static bool grounded;
+
+//    void OnTriggerEnter(Collider coll)
+//    {
+//        if(coll.gameObject.layer == 9)
+//        {
+//            grounded = true;
+//            print("true");
+//        }
+
+//    }
+//    void OnTriggerExit(Collider coll)
+//    {
+//        if (coll.gameObject.layer == 9)
+//        {
+//            grounded = false;
+//            print("false");
+//        }
+
+//    }
+//}
